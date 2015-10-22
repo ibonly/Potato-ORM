065fca58787a836a47243a8e93325270859d03ae
andela-iadeniyi <ibonly01@gmail.com>
2015-10-22 18:46:47 +0100

    Modify Travis test

--- test/ModelTest.php
+++ test/ModelTest.php
@@ -16,8 +16,8 @@ class ModelTest extends \PHPUnit_Framework_TestCase
     private $pdo;
     public function setUp()
     {
-        $this->pdo = new \PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->query("CREATE TABLE users (what VARCHAR(50) NOT NULL)");
     }
     public function tearDown()
@@ -31,14 +31,14 @@ class ModelTest extends \PHPUnit_Framework_TestCase
 
     public function testGetAll()
     {
-        $dbConnMocked = \Mockery::mock('Ibonly\SugarORM\DatabaseQuery');
-        $statement = \Mockery::mock('\PDOStatement');
+        $dbConnMocked = Mockery::mock('Ibonly\SugarORM\DatabaseQuery');
+        $statement = Mockery::mock('\PDOStatement');
 
         // $dbConnMocked->shouldReceive('query')->with('SELECT 1 FROM dogs LIMIT 1')->andReturn($statement);
         $dbConnMocked->shouldReceive('query')->with('SELECT 1 FROM users LIMIT 1')->andReturn(true);
 
         // $this->assertTrue(Backbone::checkForTable('dogs', $dbConnMocked));
-        $this->assertTrue(\Model::checkTableExist('children'));
+        $this->assertTrue(Model::checkTableExist('children'));
     }
 
 }
