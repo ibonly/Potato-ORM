8475fa1928590d9bf04039de08b38c00fed1a640
andela-iadeniyi <ibonly01@gmail.com>
2015-11-20 15:32:08 +0100

    Add getData, all and getCount method

---
+++ src/Helper/GetData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ibonly\PotatoORM;
+
+use Ibonly\PotatoORM\GetDataInterface;
+
+class GetData implements GetDataInterface
+{
+    protected $value;
+
+    function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function all()
+    {
+        return json_encode($this->value);
+    }
+
+    public function getData($name)
+    {
+        foreach (json_decode($this->all()) as $key) {
+             return $key->$name;
+        }
+   }
+
+   public function getCount()
+   {
+        return sizeof($this->value);
+   }
+}
---
+++ src/Interface/GetDataInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Defining Interface for class GetData.
+ *
+ * @package Ibonly\PotatoORM\ModelInterface
+ * @author  Ibraheem ADENIYI <ibonly01@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ */
+
+namespace Ibonly\PotatoORM;
+
+interface GetDataInterface
+{
+    public function all();
+
+    public function getData($name);
+
+    public function getCount();
+}
